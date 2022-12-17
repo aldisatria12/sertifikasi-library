e6c86ef08aa9349ffb8764a9bea6c80f5b1a13ed
aldisatria12 <aldisatria253@gmail.com>
2022-12-17 17:27:27 +0700

    init code

---
+++ sertifikasi-library/sertifikasi_libraryApp.swift
@@ -0,0 +1,20 @@
+//
+//  sertifikasi_libraryApp.swift
+//  sertifikasi-library
+//
+//  Created by Aldi Mahotma on 17/12/22.
+//
+
+import SwiftUI
+
+@main
+struct sertifikasi_libraryApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
